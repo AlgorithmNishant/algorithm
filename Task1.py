5e48d36994666842accf152022ff3e6c1bb21d06
AlgorithNishant <nishant81299tiwari@gmail.com>
2026-03-22 23:10:07 +0530

    Adding initial code.

---
+++ Task1.py
@@ -0,0 +1,23 @@
+#Task 1
+
+No1 = int(input("Enter the No1: "))
+No2 = int(input("Enter the No2: "))
+Addition = No1 + No2
+Subtraction = No1 - No2
+Multiplication = No1 * No2
+Division = No1 / No2
+
+print("Addition: ",Addition)
+print("Subtraction: ",Subtraction)
+print("Multiplication: ",Multiplication)
+print("Division: ",Division)
+
+
+
+#Task 2
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello", first_name + " " + last_name,"! Welcome to Python program")
+
