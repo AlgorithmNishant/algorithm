550cd8eca7baf446dbe7cf7e8debbe512ca10e4d
AlgorithNishant <nishant81299tiwari@gmail.com>
2026-03-22 23:26:23 +0530

    Adding doubt.py

---
+++ doubt.py
@@ -0,0 +1,23 @@
+#average of 5 numbers
+to = 0
+for i in range(5):
+    nn = 10 #float(input("Enter a No: "))
+    to += nn
+average = to / 5
+print("average: ", average)
+
+
+# Factorial of a number
+num = 8 #int(input("Enter a number: "))
+fact = 1
+
+for i in range(1, num + 1):
+    fact = fact * i
+
+print("Factorial is:", fact)
+
+
+
+
+
+#Complex Number
